6c821d8e94b459a7ea18b7b56dd11c48eeb2ff51
Nguyen Van Thai <nvthai2904@gmail.com>
2025-12-28 21:11:31 +0700

    fix(about-page): fix about bg

--- apps/frontend/src/assets/index.ts
+++ apps/frontend/src/assets/index.ts
@@ -1,5 +1,6 @@
 import authBg from "../assets/auth-bg.png";
 import heroBg from "../assets/hero-bg.jpg";
 import teamPlaySport from "../assets/team-play-sport.jpg";
+import aboutBg from "../assets/about-bg.jpg";
 
-export { authBg, heroBg, teamPlaySport };
+export { authBg, heroBg, teamPlaySport, aboutBg };
--- apps/frontend/src/pages/AboutPage.tsx
+++ apps/frontend/src/pages/AboutPage.tsx
@@ -10,14 +10,17 @@ import {
   Star,
 } from "lucide-react";
 import { Link } from "react-router-dom";
-import { teamPlaySport } from "@/assets";
+import { teamPlaySport, aboutBg } from "@/assets";
 
 export function AboutPage() {
   return (
     <div className="min-h-screen">
       {/* Hero Section */}
       <section className="relative bg-linear-to-br from-blue-900 via-blue-800 to-blue-900 text-white py-24 md:py-32 overflow-hidden">
-        <div className="absolute inset-0 bg-[url('https://images.unsplash.com/photo-1461896836934-ffe607ba8211?q=80&w=2070')] bg-cover bg-center opacity-10"></div>
+        <div
+          className="absolute inset-0  bg-cover bg-center opacity-10"
+          style={{ backgroundImage: `url(${aboutBg})` }}
+        ></div>
         <div className="container mx-auto px-4 text-center relative z-10">
           <h1 className="text-4xl md:text-6xl font-extrabold mb-6 tracking-tight">
             Về T-Sport
